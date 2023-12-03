d44e5e3d3374be1e095dce81ce471332bcbd2982
Andrew Racette <aracette@mrpsolutions.com>
2023-12-03 18:09:18 -0500

    test

---
+++ 1.py
@@ -0,0 +1,54 @@
+class playerCharacter:
+
+    def __init__(self) -> None:
+        self.name = ""
+        self.tier = 0
+        self.xp = 0
+        self.might = 0
+        self.speed = 0
+        self.intellect = 0
+        self.type = self.type()
+        self.descriptor = self.descriptor()
+        self.focus =self.focus()
+        self.armor = self.armor()
+        self.weapon = self.weapon()
+        self.cyphers = self.cyphers()
+
+#sdlkfjhasdlkfjhasdfasd
+
+class type:
+    def __init__(self) -> None:
+        pass
+
+class descriptor:
+    def __init__(self) -> None:
+        pass
+
+class focus:
+    def __init__(self) -> None:
+        pass
+
+class equipment:
+    def __init__(self) -> None:
+        pass
+
+class armor:
+    def __init__(self) -> None:
+        pass
+
+class weapon:
+    def __init__(self) -> None:
+        pass
+
+class skills:
+    def __init__(self) -> None:
+        pass
+
+class cyphers:
+    def __init__(self) -> None:
+        pass
+
+class artifacts:
+    def __init__(self) -> None:
+        pass
+
